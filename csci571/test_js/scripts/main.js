35a6744b18c23f10d3eba77520fafb1187c52d2f
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2021-02-04 19:57:25 -0800

    add

--- csci571/test_js/scripts/main.js
+++ csci571/test_js/scripts/main.js
@@ -1,3 +1,19 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-console.log('Hey bitchessss');
+let messages = [
+  "You are worthy of good things.",
+  "Every part of you is beautiful.",
+  "You are amazing!",
+  "You deserve the best.",
+  "You are enough.",
+  "Everything is going to be OK.",
+  "No matter what, you can make the most of today.",
+  "You can learn anything you set your mind to.",
+  "You will overcome all challenges.",
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function showMessage() {
+  alert(messages[getRandomInt(messages.length)]);
+}
