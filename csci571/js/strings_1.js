7bc79140b08a68c436d631aa98a3f0e34429fe30
Erica Xia <ericaxia3@gmail.com>
2021-02-12 10:50:20 -0800

    add

---
+++ csci571/js/strings_1.js
@@ -0,0 +1,8 @@
+let mystring = "coffee";
+let msg_1 = "You enter the string \'" + mystring + "\' with a length of " + mystring.length;
+console.log(msg_1);
+// console.log(mystring.indexOf("ee"))
+let substr1 = mystring.substring(0,3);
+console.log(substr1);
+let mydate = new Date();
+console.log(mydate);
