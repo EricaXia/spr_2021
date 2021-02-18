54175bf9b66aef5b938528edc630a07f76822d82
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2021-02-18 15:23:02 -0800

    add

--- csci571/hw6/hw6/main.py
+++ csci571/hw6/hw6/main.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Blueprint, render_template, Flask, request, make_response, redirect, url_for
+
+# main = Blueprint('main', __name__)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+# Main page
+# @main.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # write any relevant py code here
+    return render_template('index.html')
