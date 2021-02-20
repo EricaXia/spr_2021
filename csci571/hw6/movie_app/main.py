025c8cf688c698abbf1a3c35f418b0e5a1da3237
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2021-02-19 19:09:31 -0800

    add

--- csci571/hw6/movie_app/main.py
+++ csci571/hw6/movie_app/main.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, Flask, request, make_response, redirect, url_for
 import requests
 
-# main = Blueprint('main', __name__)
+main = Blueprint('main', __name__)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Main page
-# @main.route('/', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
 def index():
     api_key = 'a0f44b5888d8f94e608f47c1eb5575a4' 
     # write any relevant py code here
