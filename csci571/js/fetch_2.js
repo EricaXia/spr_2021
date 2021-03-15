43dd028ce903c75f96df76b2b70e3d36e06833c6
Erica Xia <ericaxia3@gmail.com>
2021-03-15 16:28:25 -0700

    add

--- csci571/js/fetch_2.js
+++ csci571/js/fetch_2.js
@@ -2,6 +2,17 @@ const fetch = require('node-fetch');
 
 let dogs_url = "https://dog.ceo/api/breed/hound/afghan/images";
 
+/* Fetch response metadata */
+fetch(dogs_url).then(function(response) {
+    console.log(response.headers.get('Content-Type'));
+    console.log(response.headers.get('Date'));
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.type);
+    console.log(response.url);
+});
+
+/* Fetch response data */
 fetch(dogs_url)
     .then(
         function (response) {
@@ -17,3 +28,5 @@ fetch(dogs_url)
     .catch(function (err) {
         console.log('Fetch Error :-S', err);
     });
+
+
