05c3fffe4857311db470895fedde73ffe2216224
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2021-03-24 17:05:59 -0700

    create components and add home component to main page

--- csci571/hw8/movie-app/src/app/app-routing.module.ts
+++ csci571/hw8/movie-app/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { fromEventPattern } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { TvComponent } from './tv/tv.component';
+import { MylistComponent } from './mylist/mylist.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'movie/:id',
+    component: MovieComponent,
+  },
+  {
+    path: 'tv/:id',
+    component: TvComponent,
+  },
+  {
+    path: 'mylist',
+    component: MylistComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- csci571/hw8/movie-app/src/app/app.module.ts
+++ csci571/hw8/movie-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { TvComponent } from './tv/tv.component';
+import { MylistComponent } from './mylist/mylist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    TvComponent,
+    MylistComponent
   ],
   imports: [
     BrowserModule,
