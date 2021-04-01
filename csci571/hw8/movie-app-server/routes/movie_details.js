c3c3008204567f674fbfbbed502ec4abb9fe0230
Erica Xia <ericaxia3@gmail.com>
2021-04-01 15:21:46 -0700

    transform cast arr before sending to frontend

--- csci571/hw8/movie-app-server/routes/movie_details.js
+++ csci571/hw8/movie-app-server/routes/movie_details.js
@@ -60,45 +60,59 @@ movieDetailsRouter.get("/", (req, res) => {
     "&language=en-US&page=1";
   let sim = axios.get(sim_url);
 
-/* TODO: look up from HW requirements to specify which pieces of each JSON (e.g. name, rating, video url, etc) we need to pass to the frontend client. We can use the .map(({attribute_names}) => ({attribute_names})) method */
-axios
-  .all([details, video, cast, reviews, recs, sim])
-  .then(
-    axios.spread((...responses) => {
-      const details2 = responses[0].data; // add ".map() method to specify which k:v pairs to get"
-      // console.log(details2);
+  /* TODO: look up from HW requirements to specify which pieces of each JSON (e.g. name, rating, video url, etc) we need to pass to the frontend client. We can use the .map(({attribute_names}) => ({attribute_names})) method */
+  axios
+    .all([details, video, cast, reviews, recs, sim])
+    .then(
+      axios.spread((...responses) => {
+        const details2 = responses[0].data; // add ".map() method to specify which k:v pairs to get"
+        // console.log(details2);
 
-      const video2 = responses[1].data;
-      // console.log(video2);
+        const video2 = responses[1].data;
+        // console.log(video2);
 
-      const cast2 = responses[2].data.cast;
+        const cast2 = responses[2].data.cast;
+        let cast3 = [];
 
-      console.log(cast2);
-      /* 
-      remove the element in the array if the element['prof_path'] value is empty */
+        // console.log(cast2);
+        /* 
+        remove the element in the array if the element['prof_path'] value is empty */
+        for (var i = 0; i < cast2.length; i++) {
+          if (cast2[i]["profile_path"]) {
+            console.log(cast2[i]["name"], cast2[i]["profile_path"])
+            cast2[i]["img_path"] = "https://image.tmdb.org/t/p/w500" + cast2[i]["profile_path"];
+            cast3.push({
+              "img_path": cast2[i]["img_path"],
+              "name": cast2[i]["name"],
+              "character": cast2[i]["character"],
+              "id": cast2[i]["id"]
+            });
+          }}
+
+        
 
 
-      //TODO: where to get reviewer user's profile pic?
-      const reviews2 = responses[3].data;
-      // console.log(reviews2);
+        //TODO: where to get reviewer user's profile pic?
+        const reviews2 = responses[3].data;
+        // console.log(reviews2);
 
-      const recs2 = responses[4].data;
-      // console.log(recs2);
+        const recs2 = responses[4].data;
+        // console.log(recs2);
 
-      const sim2 = responses[5].data;
-      // console.log(sim2);
+        const sim2 = responses[5].data;
+        // console.log(sim2);
 
-      // Send everything in one json to the client
-      res.json({
-        details: details2,
-        video: video2,
-        cast: cast2,
-        reviews: reviews2,
-        recommended: recs2,
-        similar: sim2,
-      });
-    })
-  )
+        // Send everything in one json to the client
+        res.json({
+          details: details2,
+          video: video2,
+          cast: cast3, //cast2
+          reviews: reviews2,
+          recommended: recs2,
+          similar: sim2,
+        });
+      })
+    )
     .catch((error) => console.log(error));
 });
 
